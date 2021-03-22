d84914e5c8f7738d8dd6383edb6770c771434369
musjib999 <musjib999@gmail.com>
2021-03-22 15:56:20 +0100

    changes

---
+++ lib/screens/info_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class InfoScreen extends StatefulWidget {
+  @override
+  _InfoScreenState createState() => _InfoScreenState();
+}
+
+class _InfoScreenState extends State<InfoScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Info Screen'),
+      ),
+    );
+  }
+}
