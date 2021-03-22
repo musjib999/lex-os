cea3c29dc4ff6765460b727a6f016f152c9b5a42
sadeequekeks <iazubair.ug@atbu.edu.ng>
2021-03-22 18:04:50 +0100

    changes

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -41,7 +41,7 @@ class _HomeState extends State<Home> {
                 SizedBox(
                   width: 8,
                 ),
-                RaisedButton(
+                MaterialButton(
                   // color: Colors.blue,
                   onPressed: () {
                     _location.getCurrentLocation();
